f01806635fd1d5c6d7b161a596dcc438b8324a7a
Martin Elsner Ramos <martin.elsner@gmail.com>
2015-08-11 11:57:43 -0300

    Fixes script modified.

--- osx/fixes.sh
+++ osx/fixes.sh
@@ -1,21 +1,29 @@
 #!/bin/sh
 # This script fixes some glitches in the build process.
 # Hopefully it will not be necessary anymore soon.
-# Call it from the build directory.
+# Run it in the build directory inside the build machine.
 
 set -e
 
 build_dir=$(pwd)
 
+echo 'Replace python34.zip.'
+cp /vagrant/osx/lib/python34.zip $build_dir/dist/Deskbox.app/Contents/Resources/lib
+
+echo 'Put translations in place.'
+
 # Cura
-cd $build_dir/Cura-prefix/src/Cura/resources/i18n/pt
-msgfmt cura.po -o cura.mo
-msgfmt fdmprinter.json.po -o fdmprinter.json.mo
-lconvert -i cura_qt.po -o cura_qt.ts -target-language pt
-lrelease -compress -silent cura_qt.ts -qm cura_qt.qm
-cp * $build_dir/dist/
+for lang_dir in $build_dir/Cura-prefix/src/Cura/resources/i18n/*/; do
+    cd $lang_dir
+    msgfmt cura.po -o cura.mo
+    msgfmt fdmprinter.json.po -o fdmprinter.json.mo
+    lconvert -i cura_qt.po -o cura_qt.ts -target-language pt
+    lrelease -compress -silent cura_qt.ts -qm cura_qt.qm
+done
+cp -rv $build_dir/Cura-prefix/src/Cura/resources/i18n/* $build_dir/dist/Deskbox.app/Contents/Resources/cura/resources/i18n
 
 # Uranium
 cd $build_dir/Uranium-prefix/src/Uranium-build
 make translations
 make copy-translations
+cp -rv ?? $build_dir/dist/Deskbox.app/Contents/Resources/uranium/resources/i18n/
