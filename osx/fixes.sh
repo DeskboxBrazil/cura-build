1a1d1cd2b706ada456481e269b58a79ede541155
Martin Elsner Ramos <martin.elsner@gmail.com>
2015-08-12 13:34:25 -0300

    Improved fixes script

--- osx/fixes.sh
+++ osx/fixes.sh
@@ -13,26 +13,24 @@ cp /vagrant/osx/lib/python34.zip $build_dir/dist/Deskbox.app/Contents/Resources/
 echo 'Put translations in place.'
 
 # Cura
-for lang_dir in $build_dir/Cura-prefix/src/Cura/resources/i18n/*/; do
-    echo $lang_dir
-    cd $lang_dir
-    msgfmt cura.po -o cura.mo
-    msgfmt fdmprinter.json.po -o fdmprinter.json.mo
-    lconvert -i cura_qt.po -o cura_qt.ts -target-language $lang_dir
-    lrelease -compress -silent cura_qt.ts -qm cura_qt.qm
-    compiled_lang_dir=$build_dir/dist/Deskbox.app/Contents/Resources/cura/resources/i18n/$lang_dir/LC_MESSAGES
-    mkdir -p $compiled_lang_dir
-    cp -rv * $compiled_lang_dir
-done
+cp -rv $build_dir/inst/share/cura/resources/i18n $build_dir/dist/Deskbox.app/Contents/Resources/cura/resources
+# Build pt
+cd $build_dir/Cura-prefix/src/Cura/resources/i18n/pt
+compiled_lang_dir=$build_dir/dist/Deskbox.app/Contents/Resources/cura/resources/i18n/pt/LC_MESSAGES
+mkdir -p $compiled_lang_dir
+msgfmt cura.po -o $compiled_lang_dir/cura.mo
+msgfmt fdmprinter.json.po -o $compiled_lang_dir/fdmprinter.json.mo
+lconvert -i cura_qt.po -o cura_qt.ts -target-language pt
+lrelease -compress -silent cura_qt.ts -qm $compiled_lang_dir/cura_qt.qm
 
 # Uranium
-cd $build_dir/Uranium-prefix/src/Uranium-build
-make translations
-make copy-translations
-for lang_dir in ??/; do
-    compiled_lang_dir=$build_dir/dist/Deskbox.app/Contents/Resources/uranium/resources/i18n/$lang_dir/LC_MESSAGES
-    mkdir -p $compiled_lang_dir
-    cp -rv $lang_dir/* $compiled_lang_dir
-done
+cp -rv $build_dir/inst/share/uranium/resources/i18n $build_dir/dist/Deskbox.app/Contents/Resources/uranium/resources
+# Build pt
+cd $build_dir/Uranium-prefix/src/Uranium/resources/i18n/pt
+compiled_lang_dir=$build_dir/dist/Deskbox.app/Contents/Resources/uranium/resources/i18n/pt/LC_MESSAGES
+mkdir -p $compiled_lang_dir
+msgfmt uranium.po -o $compiled_lang_dir/uranium.mo
+lconvert -i uranium_qt.po -o uranium_qt.ts -target-language pt
+lrelease -compress -silent uranium_qt.ts -qm $compiled_lang_dir/uranium_qt.qm
 
 echo "Fixes applied."
